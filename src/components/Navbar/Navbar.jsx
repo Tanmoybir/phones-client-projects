7cfb51d8e9e9d31521768ec1eca085ad0bb9de71
Tanmoy Bir <tanmoy7468@gmail.com>
2023-12-22 20:07:47 +0600

    make naver and responsive it

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { CiMenuBurger } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
 
     const navLinks = <>
-        <li>
+        <li className="">
             <NavLink
                 to="/"
                 className={({ isActive, isPending }) =>
-                    isPending ? "pending" : isActive ? "text-2xl bg-slate-200 text-green-700" : "hover:text-slate-100"
+                    isPending ? "pending" : isActive ? "text-2xl px-3 py-1 bg-slate-200 text-green-700" : "text-2xl hover:bg-slate-100 px-3 py-1"
                 }
             >
                 Home
@@ -17,7 +21,7 @@ const Navbar = () => {
             <NavLink
                 to="/phones"
                 className={({ isActive, isPending }) =>
-                    isPending ? "pending" : isActive ? "text-2xl bg-slate-200 text-green-700" : "hover:text-slate-100"
+                    isPending ? "pending" : isActive ? "text-2xl px-3 py-1 bg-slate-200 text-green-700" : "text-2xl hover:bg-slate-100 px-3 py-1"
                 }
             >
                 Phones
@@ -27,7 +31,7 @@ const Navbar = () => {
             <NavLink
                 to="/about"
                 className={({ isActive, isPending }) =>
-                    isPending ? "pending" : isActive ? "text-2xl bg-slate-200 text-green-700" : "hover:text-slate-100"
+                    isPending ? "pending" : isActive ? "text-2xl px-3 py-1 bg-slate-200 text-green-700" : "text-2xl hover:bg-slate-100 px-3 py-1"
                 }
             >
                 About
@@ -36,13 +40,43 @@ const Navbar = () => {
     </>
     return (
         <div>
-            <nav className="flex justify-between items-center">
-                <h1>Phone</h1>
-                <ul className="flex justify-between items-center gap-5">
-                    {navLinks}
-                </ul>
-            </nav>
-        </div>
+            <nav className="flex justify-between items-center relative shadow-lg">
+                <div className="flex items-center gap-5">
+                    <div className="lg:hidden" onClick={() => setOpen(!open)}>
+                        { open ? <IoMdClose /> : <CiMenuBurger/>}
+                    </div>
+                    <h1>Phone</h1>
+                </div>
+                <div className="hidden lg:block">
+                    <ul className="flex justify-between items-center gap-5">
+                        {navLinks}
+                    </ul>
+                </div>
+
+                {/*  */}
+
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </div>
+                    <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+            </nav >
+            <ul className={`lg:hidden space-y-3 bg-red-200 absolute z-10 px-4 py-2 top-14 left-0 ${open ? "" : "-top-60"}`}>
+                {navLinks}
+            </ul>
+        </div >
     );
 };
 
